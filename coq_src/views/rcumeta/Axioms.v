952a9c09faae415e0e850589f43b69fc3e63a00d
ismailkuru <aptaliso@gmail.com>
2018-11-23 20:24:44 -0500

    migrating the coq_src to another repo

--- coq_src/views/rcumeta/Axioms.v
+++
@@ -1,4 +0,0 @@
-(*
-In this file we define the WellFormedness Relations
-over instrumented state.
-*)
--- coq_src/views/rcumeta/Typesystem.v
+++
@@ -1,8 +0,0 @@
-(*
-This document includes the rcu type rules that are 
-defined both for read and write critical section in 
-Figure 32 and Figure 6 respectively. Then structural
-typing rules shown in Figure 3 and partly in Figure 32
-follow
- *)
-
